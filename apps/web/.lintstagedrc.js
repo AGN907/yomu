f24aa69574bf90acd23fe898de69458596ce4443
AGN907 <itzme907@gmail.com>
2024-05-17 01:33:35 +0300

    style(web): format files

--- apps/web/.lintstagedrc.js
+++ apps/web/.lintstagedrc.js
@@ -3,10 +3,8 @@ const { relative } = require('path')
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => relative(process.cwd(), f))
-    .join(' --file ')}`;
+    .join(' --file ')}`
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-};
-
-
+}
