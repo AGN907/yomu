e1e1ba3c3a88af79de920cd2f07153087d6ca020
AGN907 <itzme907@gmail.com>
2024-05-18 01:45:31 +0300

    feat(web): create auth actions

---
+++ apps/web/src/lib/actions/auth/index.ts
@@ -0,0 +1,112 @@
+'use server'
+
+import { db } from '@/lib/database'
+import { action } from '@/lib/safe-action'
+import { loginSchema, signupSchema } from '@/lib/validators/auth'
+
+import { lucia } from '@/lib/auth'
+import { validateRequest } from '@/lib/auth/validate-request'
+
+import { hash, verify } from '@node-rs/argon2'
+import { generateIdFromEntropySize } from 'lucia'
+
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { users } from '@yomu/core/database/schema/web'
+
+export const signup = action(
+  signupSchema,
+  async ({ username, password, firstName, lastName }) => {
+    const hashedPassword = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    })
+
+    const userId = generateIdFromEntropySize(10)
+
+    const existingUser = await db.query.users.findFirst({
+      where: (table, { eq }) => eq(table.username, username),
+    })
+
+    if (!existingUser)
+      return {
+        failure: 'Invaild username',
+      }
+
+    await db.insert(users).values({
+      id: userId,
+      username: username,
+      firstName: firstName,
+      lastName: lastName,
+      hashedPassword: hashedPassword,
+    })
+
+    const session = await lucia.createSession(userId, {})
+    const sessionCookie = lucia.createSessionCookie(session.id)
+
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    )
+
+    redirect('/')
+  },
+)
+
+export const login = action(loginSchema, async ({ username, password }) => {
+  const existingUser = await db.query.users.findFirst({
+    where: (table, { eq }) => eq(table.username, username),
+  })
+
+  if (!existingUser)
+    return {
+      failure: 'Invalid username or password',
+    }
+
+  const validPassword = await verify(existingUser.hashedPassword, password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1,
+  })
+
+  if (!validPassword)
+    return {
+      failure: 'Invalid username or password',
+    }
+
+  const session = await lucia.createSession(existingUser.id, {})
+  const sessionCookie = lucia.createSessionCookie(session.id)
+
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  )
+
+  return redirect('/')
+})
+
+export const logout = async () => {
+  const { session } = await validateRequest()
+  if (!session) {
+    return {
+      error: 'Unauthorized',
+    }
+  }
+
+  await lucia.invalidateSession(session.id)
+
+  const sessionCookie = lucia.createBlankSessionCookie()
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  )
+
+  return redirect('/login')
+}
---
+++ apps/web/src/lib/validators/auth.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod'
+
+export const signupSchema = z.object({
+  username: z
+    .string()
+    .min(4, 'Please provider your username')
+    .max(31, 'Username is too long. Maximum length is 31 characters')
+    .regex(
+      /^[a-zA-Z0-9_-]+$/,
+      'Username must use alphanumeric characters with dash and underscore only',
+    ),
+  firstName: z
+    .string()
+    .min(4, 'Please provide your first name')
+    .max(31, 'First name is too long. Maximum length is 31 characters')
+    .regex(/a-zA-Z/, 'First name must only contain letters'),
+  lastName: z
+    .string()
+    .min(4, 'Please provide your last name')
+    .max(31, 'Last name is too long. Maximum length is 31 characters')
+    .regex(/a-zA-Z/, 'Last name must only contain letters'),
+  password: z
+    .string()
+    .min(6, 'Password is too short. Minimum length is 6 characters')
+    .max(255),
+})
+
+export type SignupInput = z.infer<typeof signupSchema>
+
+export const loginSchema = z.object({
+  username: z
+    .string()
+    .min(4, 'Username is too short. Minimum length is 4 characters')
+    .max(31, 'Username is too long. Maximum length is 31 characters'),
+  password: z
+    .string()
+    .min(6, 'Password is too short. Minimum length is 6 characters')
+    .max(255),
+})
+
+export type LoginInput = z.infer<typeof loginSchema>
