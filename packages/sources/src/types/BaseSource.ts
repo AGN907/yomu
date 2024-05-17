2756f30a852a422d7c933cf642daa6a111a0182e
AGN907 <itzme907@gmail.com>
2024-05-17 16:29:12 +0300

    feat(sources): setup sources package

---
+++ packages/sources/src/types/BaseSource.ts
@@ -0,0 +1,75 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import ky from 'ky'
+import {
+  ChapterItemWithContent,
+  ChapterItemWithoutContent,
+  Source,
+  SourceNovelFetchResponse,
+  SourceNovelsFetchResponse,
+} from './'
+
+export abstract class BaseSource implements Source {
+  id
+  name
+  baseUrl
+  lang
+
+  httpClient
+
+  constructor(id: string, name: string, baseUrl: string, lang: string) {
+    this.id = id
+    this.name = name
+    this.baseUrl = baseUrl
+    this.lang = lang
+
+    this.httpClient = ky.create({
+      prefixUrl: baseUrl,
+    })
+  }
+
+  fetchedNovelSelector(): string {
+    throw new Error('Method not implemented.')
+  }
+  searchedNovelSelector(): string {
+    throw new Error('Method not implemented.')
+  }
+  chapterSelector(): string {
+    throw new Error('Method not implemented.')
+  }
+
+  fetchedNovelCountSelector(): string {
+    throw new Error('Method not implemented.')
+  }
+  searchedNovelCountSelector(): string {
+    throw new Error('Method not implemented.')
+  }
+
+  async fetchNovels(
+    page: number,
+    showLatest = false,
+  ): Promise<SourceNovelsFetchResponse> {
+    throw new Error('Method not implemented.')
+  }
+
+  async searchNovels(
+    page: number,
+    query: string,
+  ): Promise<SourceNovelsFetchResponse> {
+    throw new Error('Method not implemented.')
+  }
+
+  async fetchNovel(url: string): Promise<SourceNovelFetchResponse> {
+    throw new Error('Method not implemented.')
+  }
+
+  async fetchNovelChapters(
+    url: string,
+    novelId: string,
+  ): Promise<ChapterItemWithoutContent[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async fetchChapter(url: string): Promise<ChapterItemWithContent | null> {
+    throw new Error('Method not implemented.')
+  }
+}
---
+++ packages/sources/src/types/index.ts
@@ -0,0 +1,81 @@
+export * from './BaseSource'
+
+export interface NovelItem {
+  title: string
+  url: string
+  thumbnail: string
+  genres: string[]
+  summary?: string
+  author?: string
+  status: NovelStatus
+}
+
+export interface NovelItemData extends NovelItem {
+  summary: string
+  author: string
+}
+
+export enum NovelStatus {
+  COMPLETED = 'Completed',
+  ONGOING = 'Ongoing',
+  UNKNOWN = 'Unknown',
+}
+
+export interface ChapterItemWithoutContent {
+  title: string
+  url: string
+  number: number
+  releaseDate: Date
+}
+
+export interface ChapterItemWithContent
+  extends Omit<ChapterItemWithoutContent, 'releaseDate'> {
+  content: string
+}
+
+export interface HistoryItem {
+  id: number
+  novelId: number
+  chapterId: number
+  novelTitle: string
+  chapterTitle: string
+  novelUrl: string
+  chapterUrl: string
+  chapterNumber: number
+  novelThumbnail: string
+  sourceId: string
+}
+
+export type UpdateItem = HistoryItem
+
+export interface SourceNovelsFetchResponse {
+  novels: NovelItem[]
+  hasNextPage: boolean
+}
+
+export interface SourceNovelFetchResponse {
+  novel: NovelItemData | null
+  chapters: ChapterItemWithoutContent[]
+}
+
+export interface SourceInfo {
+  id: string
+  name: string
+  baseUrl: string
+  icon: string
+  lang: string
+}
+
+export interface Source {
+  fetchNovels: (
+    page: number,
+    showLatest?: boolean,
+  ) => Promise<SourceNovelsFetchResponse>
+  searchNovels: (
+    page: number,
+    query: string,
+  ) => Promise<SourceNovelsFetchResponse>
+  fetchNovel: (url: string) => Promise<SourceNovelFetchResponse>
+
+  fetchChapter: (url: string) => Promise<ChapterItemWithContent | null>
+}
