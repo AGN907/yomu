fb2cc029c8f8a80df58cd406f1d4f5490ee161b2
AGN907 <itzme907@gmail.com>
2024-05-18 23:42:37 +0300

    refactor(icons): rexport used icons only

--- packages/ui/src/components/ui/icons.tsx
+++ packages/ui/src/components/ui/icons.tsx
@@ -1,1 +1,33 @@
-export * from 'lucide-react'
+import {
+  ArrowRight,
+  BadgeAlert,
+  BookOpen,
+  BookOpenCheck,
+  BookText,
+  Compass,
+  HistoryIcon,
+  Home,
+  Library,
+  Loader2 as Loader,
+  Menu,
+  Moon,
+  Settings,
+  Sun,
+} from 'lucide-react'
+
+export {
+  ArrowRight,
+  BadgeAlert,
+  BookOpen,
+  BookOpenCheck,
+  BookText,
+  Compass,
+  HistoryIcon,
+  Home,
+  Library,
+  Loader,
+  Menu,
+  Moon,
+  Settings,
+  Sun,
+}
