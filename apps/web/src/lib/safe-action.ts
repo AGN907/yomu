3fd960a351092d0eba2ce4104f7307038c072254
AGN907 <itzme907@gmail.com>
2024-06-04 01:03:14 +0300

    feat(web): add action client with auth

--- apps/web/src/lib/safe-action.ts
+++ apps/web/src/lib/safe-action.ts
@@ -1,3 +1,13 @@
+import { getUserOrRedirect } from './actions/auth'
+
 import { createSafeActionClient } from 'next-safe-action'
 
 export const action = createSafeActionClient()
+
+export const actionWithAuth = createSafeActionClient({
+  async middleware(parsedInput, data) {
+    const user = await getUserOrRedirect()
+
+    return { userId: user.id }
+  },
+})
