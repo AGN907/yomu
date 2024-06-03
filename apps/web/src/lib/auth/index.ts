a7edd109343ec09213f4f2425d149e25c8a54997
AGN907 <itzme907@gmail.com>
2024-06-04 01:12:57 +0300

    refactor(web): remove ts inline expect error

--- apps/web/src/lib/auth/index.ts
+++ apps/web/src/lib/auth/index.ts
@@ -1,10 +1,10 @@
 import { db } from '@/lib/database'
 
-import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle'
 import { sessions, users } from '@yomu/core/database/schema/web'
+
+import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle'
 import { Lucia } from 'lucia'
 
-// @ts-expect-error - Both drizzle and lucia have separate delclarations of private properties
 const adapter = new DrizzleSQLiteAdapter(db, sessions, users)
 
 export const lucia = new Lucia(adapter, {
