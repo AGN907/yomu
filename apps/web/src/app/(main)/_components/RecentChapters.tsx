770039062cf0ab30749f3a41e25dca9e8c862168
AGN907 <itzme907@gmail.com>
2024-06-08 01:16:45 +0300

    refactor(web): show message if there's no recent chapters & fix hardcoded thumbnail

--- apps/web/src/app/(main)/_components/RecentChapters.tsx
+++ apps/web/src/app/(main)/_components/RecentChapters.tsx
@@ -9,36 +9,24 @@ import Link from 'next/link'
 export async function RecentChapters() {
   const recentChapters = await getLatestUpdatedChapters(5)
 
+  const isRecentChaptersEmpty = recentChapters.length === 0
+
   return (
     <div className="">
       <CardContainer title={'Recent Chapters'}>
-        <div className="divide-border divide-y">
-          {recentChapters.map((chapter) => (
-            <div
-              className="grid grid-cols-[40px_1fr] items-center gap-2 py-2"
-              key={chapter.novelId}
-            >
-              <Link
-                className="block hover:underline"
-                href={{
-                  pathname: '/novel',
-                  query: {
-                    novelUrl: chapter.novelUrl,
-                    sourceId: chapter.sourceId,
-                  },
-                }}
+        <div className="divide-border h-full divide-y">
+          {isRecentChaptersEmpty ? (
+            <div className="flex items-center justify-center">
+              <p>No recent chapters</p>
+            </div>
+          ) : (
+            recentChapters.map((chapter) => (
+              <div
+                className="grid grid-cols-[40px_1fr] items-center gap-2 py-1"
+                key={chapter.chapterId}
               >
-                <Image
-                  className="rounded"
-                  src={'/lord-of-the-mysteries-2.jpg'}
-                  alt={chapter.novelTitle}
-                  width={30}
-                  height={40}
-                />
-              </Link>
-              <div className="flex w-full min-w-0 flex-col">
                 <Link
-                  className="truncate font-semibold hover:underline"
+                  className="block hover:underline"
                   href={{
                     pathname: '/novel',
                     query: {
@@ -47,24 +35,44 @@ export async function RecentChapters() {
                     },
                   }}
                 >
-                  {chapter.novelTitle}
+                  <Image
+                    className="rounded"
+                    src={chapter.novelThumbnail}
+                    alt={chapter.novelTitle}
+                    width={50}
+                    height={80}
+                  />
                 </Link>
+                <div className="flex w-full min-w-0 flex-col">
+                  <Link
+                    className="truncate font-medium hover:underline"
+                    href={{
+                      pathname: '/novel',
+                      query: {
+                        novelUrl: chapter.novelUrl,
+                        sourceId: chapter.sourceId,
+                      },
+                    }}
+                  >
+                    {chapter.novelTitle}
+                  </Link>
 
-                <Link
-                  className="truncate text-sm hover:underline"
-                  href={{
-                    pathname: `/novel/${chapter.chapterNumber}`,
-                    query: { chapterId: chapter.chapterId },
-                  }}
-                >
-                  {chapter.chapterTitle}
-                </Link>
-                <span className="text-muted-foreground ml-auto flex-shrink-0 text-xs">
-                  {formatReleaseDate(chapter.updatedAt)}
-                </span>
+                  <Link
+                    className="truncate text-sm hover:underline"
+                    href={{
+                      pathname: `/novel/${chapter.chapterNumber}`,
+                      query: { chapterId: chapter.chapterId },
+                    }}
+                  >
+                    {chapter.chapterTitle}
+                  </Link>
+                  <span className="text-muted-foreground ml-auto flex-shrink-0 text-xs">
+                    {formatReleaseDate(chapter.updatedAt)}
+                  </span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </CardContainer>
     </div>
