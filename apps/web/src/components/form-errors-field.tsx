fb1e816ebb121141105a43065f4b51a2c78518bb
AGN907 <itzme907@gmail.com>
2024-06-23 16:31:14 +0300

    fix(web): form errors shown as array

--- apps/web/src/components/form-errors-field.tsx
+++ apps/web/src/components/form-errors-field.tsx
@@ -13,11 +13,13 @@ function FormErrorsField({ result }: FormErrorsFieldProps) {
     <div>
       {result?.validationErrors ? (
         <ul className="bg-destructive/10 text-destructive list-disc space-y-1 rounded-lg border p-2 text-[0.8rem] font-medium">
-          {Object.values(result.validationErrors).flatMap((err, index) => (
-            <li className="ml-4" key={index}>
-              {err}
-            </li>
-          ))}
+          {Object.values(result.validationErrors)
+            .flat()
+            .map((err, index) => (
+              <li className="ml-4" key={index}>
+                {err}
+              </li>
+            ))}
         </ul>
       ) : result?.data?.error ? (
         <p className="bg-destructive/10 text-destructive rounded-lg border p-2 text-[0.8rem] font-medium">
