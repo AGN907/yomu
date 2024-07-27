62d2afc6b32fdb1017edeff26b8866a01468e500
AGN907 <itzme907@gmail.com>
2024-07-28 01:46:57 +0300

    feat(web): add search on typing for browsing novels

---
+++ apps/web/src/lib/hooks/use-debounced.ts
@@ -0,0 +1,14 @@
+function useDebounced<T>(func: (...args: T[]) => void, delay = 500) {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null
+
+  return function (...args: T[]) {
+    clearTimeout(timeoutId || undefined)
+
+    timeoutId = setTimeout(() => {
+      timeoutId = null
+      func.apply(this, args)
+    }, delay)
+  }
+}
+
+export { useDebounced }
