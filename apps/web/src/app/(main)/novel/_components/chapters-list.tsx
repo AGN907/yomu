cf70ec9497ae85c532339db83699f4f804e96b5a
AGN907 <itzme907@gmail.com>
2024-07-28 19:52:04 +0300

    feat(web): mark multiple chapters as read

--- apps/web/src/app/(main)/novel/_components/chapters-list.tsx
+++ apps/web/src/app/(main)/novel/_components/chapters-list.tsx
@@ -1,12 +1,11 @@
 'use client'
 
-import type { Chapter } from '@yomu/core/database/schema/web'
-import { toCalendar } from '@yomu/core/date-helpers'
-import { Input } from '@yomu/ui/components/input'
-import { Label } from '@yomu/ui/components/label'
-import { cn } from '@yomu/ui/utils'
+import { markChapterAsRead } from '@/lib/actions/chapters'
+import { Chapter } from '@yomu/core/database/schema/web'
+import { Button } from '@yomu/ui/components/button'
 
-import Link from 'next/link'
+import { ChapterItem } from './chapter-item'
+
 import { useState } from 'react'
 
 type ChaptersListProps = {
@@ -14,64 +13,48 @@ type ChaptersListProps = {
 }
 
 function ChaptersList({ chapters }: ChaptersListProps) {
-  const [checkedIds, setCheckedIds] = useState<Set<number>>(new Set())
+  const [selectedIds, setSelectedIds] = useState<Set<number>>(new Set())
 
   const handleCheck = (id: number) => {
-    const newCheckedIds = new Set(checkedIds)
+    const newCheckedIds = new Set(selectedIds)
     if (newCheckedIds.has(id)) {
       newCheckedIds.delete(id)
     } else {
       newCheckedIds.add(id)
     }
 
-    setCheckedIds(newCheckedIds)
+    setSelectedIds(newCheckedIds)
+  }
+
+  const markAsRead = async () => {
+    if (selectedIds.size === 0) return
+
+    await markChapterAsRead({
+      chapterIds: [...selectedIds],
+    })
+
+    setSelectedIds(new Set())
   }
 
   return (
     <div className="space-y-4">
-      {checkedIds.size > 0 ? (
+      {selectedIds.size > 0 ? (
         <div className="bg-accent flex items-center justify-between rounded border px-2 py-1">
-          <p className="text-sm">{checkedIds.size} chapter selected</p>
+          <p className="text-sm">{selectedIds.size} chapter selected</p>
+          <div className="flex gap-4">
+            <Button onClick={markAsRead} variant="outline" size="sm">
+              Mark read
+            </Button>
+          </div>
         </div>
       ) : null}
       {chapters.map((chapter) => (
-        <Label
+        <ChapterItem
           key={chapter.id}
-          className="bg-card group flex items-center gap-4 rounded border px-2 py-4"
-        >
-          <Input
-            onChange={() => handleCheck(chapter.id)}
-            checked={checkedIds.has(chapter.id)}
-            type="checkbox"
-            className={cn(
-              'max-w-5 opacity-0 transition-opacity delay-300',
-              checkedIds.has(chapter.id)
-                ? 'opacity-100'
-                : 'group-hover:opacity-100',
-            )}
-          />
-          <div className="flex flex-1 justify-between">
-            <Link
-              key={chapter.id}
-              href={{
-                pathname: `/novel/${chapter.number}`,
-                query: {
-                  chapterId: chapter.id,
-                },
-              }}
-              title={chapter.title}
-              className={cn(
-                'hover:underline',
-                chapter.read && 'text-muted-foreground',
-              )}
-            >
-              {chapter.title}
-            </Link>
-            <span className="text-muted-foreground text-sm">
-              {toCalendar(chapter.releaseDate.toDateString())}
-            </span>
-          </div>
-        </Label>
+          chapter={chapter}
+          isSelected={selectedIds.has(chapter.id)}
+          onSelect={handleCheck}
+        />
       ))}
     </div>
   )
