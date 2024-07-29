85acf046c2ffe958a5cc2881edea37b94e59f159
AGN907 <itzme907@gmail.com>
2024-07-29 17:35:28 +0300

    refactor(web): refactor chapters list

--- apps/web/src/app/(main)/novel/_components/chapter-item.tsx
+++ apps/web/src/app/(main)/novel/_components/chapter-item.tsx
@@ -16,37 +16,35 @@ function ChapterItem({ chapter, isSelected, onSelect }: ChapterItemProps) {
   const { id, title, number, read, releaseDate } = chapter
 
   return (
-    <Label
-      key={id}
-      className="bg-card group flex items-center gap-4 rounded border px-2 py-4"
-    >
+    <Label className="bg-card group grid grid-cols-[2rem_1fr_auto] items-center gap-4 rounded border px-2 py-4">
       <Input
         onChange={() => onSelect(chapter.id)}
         checked={isSelected}
         type="checkbox"
         className={cn(
-          'max-w-5 opacity-0 transition-opacity delay-300',
+          'max-w-5 opacity-0 transition-opacity delay-100',
           isSelected ? 'opacity-100' : 'group-hover:opacity-100',
         )}
       />
-      <div className="flex flex-1 justify-between">
-        <Link
-          key={id}
-          href={{
-            pathname: `/novel/${number}`,
-            query: {
-              chapterId: id,
-            },
-          }}
-          title={title}
-          className={cn('hover:underline', read && 'text-muted-foreground')}
-        >
-          {title}
-        </Link>
-        <span className="text-muted-foreground text-sm">
-          {toCalendar(releaseDate.toDateString())}
-        </span>
-      </div>
+      <Link
+        key={id}
+        href={{
+          pathname: `/novel/${number}`,
+          query: {
+            chapterId: id,
+          },
+        }}
+        title={title}
+        className={cn(
+          'truncate hover:underline',
+          read && 'text-muted-foreground',
+        )}
+      >
+        {title}
+      </Link>
+      <span className="text-muted-foreground ml-auto flex-shrink-0 text-xs">
+        {toCalendar(releaseDate.toDateString())}
+      </span>
     </Label>
   )
 }
