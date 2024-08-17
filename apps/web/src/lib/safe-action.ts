949b5ec3c4aa16b41522ef09f166f18036b50360
AGN907 <itzme907@gmail.com>
2024-08-18 00:15:10 +0300

    feat(web): shape safe-action errors

--- apps/web/src/lib/safe-action.ts
+++ apps/web/src/lib/safe-action.ts
@@ -1,3 +1,4 @@
+import { PublicError } from './errors'
 import { assertAuthenticated } from './session'
 
 import { createServerActionProcedure } from 'zsa'
@@ -5,14 +6,36 @@ import { createServerActionProcedure } from 'zsa'
 export type UserSession = {
   id: string
   username: string
-  fullName: string
+}
+
+function shapeErrors({ err }: any) {
+  const isAllowedError = err instanceof PublicError
+  const isDev = process.env.NODE_ENV === 'development'
+  if (isAllowedError || isDev) {
+    console.error(err)
+    return {
+      code: err.code ?? 'ERROR',
+      message: `${!isAllowedError && isDev ? 'DEV ONLY ENABLED - ' : ''}${
+        err.message
+      }`,
+    }
+  } else {
+    return {
+      code: 'ERROR',
+      message: 'Something went wrong',
+    }
+  }
 }
 
 export const publicAction = createServerActionProcedure()
-  .handler(async () => {})
+  .experimental_shapeError(shapeErrors)
+  .handler(async (props) => {
+    console.log(props.responseMeta?.statusCode)
+  })
   .createServerAction()
 
 export const authenticatedAction = createServerActionProcedure()
+  .experimental_shapeError(shapeErrors)
   .handler(async () => {
     const user = await assertAuthenticated()
 
