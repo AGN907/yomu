2a17dc84fa3052438ca5760205d311fd877e38ab
AGN907 <itzme907@gmail.com>
2024-08-25 23:37:33 +0300

    feat(web): add react-query devtools

--- apps/web/src/providers.tsx
+++ apps/web/src/providers.tsx
@@ -7,6 +7,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ThemeProvider } from 'next-themes'
 
 function makeQueryClient() {
@@ -40,8 +41,11 @@ function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient()
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <Toaster />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
